a96b536c7f53542ef7cde0404fe843ef4fd5cde7
Katie Morrison <katelyncmorrison98@gmail.com>
2021-10-15 16:56:19 -0400

    States choice with fires

--- main.js
+++ main.js
@@ -15,31 +15,31 @@ var currentMapYear = '2000';
 
 var colorrange = ["#7c0202", "#b64d24", "#b86213", "#e18820", "#de9b10", "#f3c523", "#7c5201", "#fac45a", "#fd860b", "#ffdc6c", "#ff4901", "#a43407"] //we can change these colors later :)
 var categories = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']
-var statesDict = {"AL": "Alabama",
-              "AK": "Alaska",
-              "AZ": "Arizona",
-              "AR": "Arkansas",
-              "CA": "California",
-              "CO": "Colorado",
-              "CT": "Connecticut",
-              "DE": "Delaware",
-              "FL": "Florida",
-              "GA": "Georgia",
-              "HI": "Hawaii",
-              "ID": "Idaho",
-              "IL": "Illinois",
-              "IN": "Indiana",
-              "IA": "Iowa",
-              "KS": "Kansas",
-              "KY": "Kentucky",
-              "LA": "Louisiana",
-              "ME": "Maine",
-              "MD": "Maryland",
-              "MA": "Massachusetts",
-              "MI": "Michigan",
-              "MN": "Minnesota",
-              "MS": "Mississippi",
-              "MO": "Missouri",
+var statesDict = {"Alabama": "AL",
+              "Alaska": "AK",
+              "Arizona": "AZ",
+              "Arkansas": "AR",
+              "California": "CA",
+              "Colorado": "CO",
+              "Connecticut": "CT",
+              "Delaware": "DE",
+              "Florida": "FL",
+              "Georgia": "GA",
+              "Hawaii": "HI",
+              "Idaho": "ID",
+              "Illinois": "IL",
+              "Indiana": "IN",
+              "Iowa": "IA",
+              "Kansas": "KS",
+              "Kentucky": "KY",
+              "Louisiana": "LA",
+              "Maine": "ME",
+              "Maryland": "MD",
+              "Massachusetts": "MA",
+              "Michigan": "MI",
+              "Minnesota": "MN",
+              "Mississippi": "MS",
+              "Missouri": "MO",
               "MT": "Montana",
               "NE": "Nebraska",
               "NV": "Nevada",
@@ -57,7 +57,7 @@ var statesDict = {"AL": "Alabama",
               "SC": "South Carolina",
               "SD": "South Dakota",
               "TN": "Tennessee",
-              "TX": "Texas",
+              "Texas": "TX",
               "UT": "Utah",
               "VT": "Vermont",
               "VA": "Virgina",
@@ -68,7 +68,57 @@ var statesDict = {"AL": "Alabama",
             }
 var states = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virgina","Washington","West Virginia","Wisconsin","Wyoming"]
 var stateacr = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
-            
+var stateProjs = {"Alabama": [-85, 30, 1000],
+ "Alaska": [-150, 60, 400],
+ "Arizona": [-107, 32, 1000],
+ "Arkansas": [-90, 30, 1000],
+ "California": [-113, 34, 1000],
+ "Colorado": [],
+ "Connecticut": [],
+ "Delaware": [],
+ "Florida": [],
+ "Georgia": [],
+ "Hawaii": [],
+ "Idaho": [],
+ "Illinois": [],
+ "Indiana": [],
+ "Iowa": [],
+ "Kansas": [],
+ "Kentucky": [],
+ "Louisiana": [],
+ "Maine": [],
+ "Maryland": [],
+ "Massachusetts": [],
+ "Michigan": [],
+ "Minnesota": [],
+ "Mississippi": [],
+ "Missouri": [],
+ "Montana": [],
+ "Nebraska": [],
+ "Nevada": [-115, 35, 1000],
+ "New Hampshire": [],
+ "New Jersey": [],
+ "New Mexico": [-103, 30, 1000],
+ "New York": [],
+ "North Carolina": [],
+ "North Dakota": [],
+ "Ohio": [],
+ "Oklahoma": [],
+ "Oregon": [],
+ "Pennsylvania": [],
+ "Rhode Island": [],
+ "South Carolina": [],
+ "South Dakota": [],
+ "Tennessee": [],
+ "Texas": [-95, 27, 1000],
+ "Utah": [],
+ "Vermont": [],
+ "Virgina": [],
+ "Washington": [],
+ "West Virginia": [],
+ "Wisconsin": [],
+ "Wyoming": []}
+      
 
 //adjusting the size of the circle for both the bubble map and the key to use
 var minRadiusRange = 1;
@@ -111,9 +161,6 @@ d3.csv("assets/firesfinaldata.csv").then((table)=>{
 
 
 d3.json("assets/geojson/USA.geojson").then(function(data){
-
-// Filter data
-//data.features = data.features.filter( function(d){return d.properties.name=="USA"});
 
   // Draw the map
   svg.append("g")
@@ -338,62 +385,128 @@ d3.json("assets/geojson/USA.geojson").then(function(data){
         .text(function (d) { return d; }) // text showed in the menu
         .attr("value", function (d) { return d; }) // corresponding value returned by the button
 
+  let element = document.getElementById("stateSelect");
+  element.value = "Texas";
+
   // Map and projection
   var projection2 = d3.geoMercator()
-    .center([-86, 27])                // GPS of location to zoom on
-    .scale(200)                       // This is like the zoom
-    .translate([ width/2, height/2 ])
+    .center([stateProjs["Texas"][0], stateProjs["Texas"][1]])                // GPS of location to zoom on
+      .scale(stateProjs["Texas"][2])                       // This is like the zoom
+      .translate([ width/2, height/2 ])
 
   console.log(data.features)
 
-  state.features = data.features.filter( function(d){return d.properties.NAME=="Alabama"} )
+  state.features = data.features.filter( function(d){return d.properties.NAME=="Texas"} )
   console.log(state.features)
 
-  var stateplot = Statesvg.selectAll("path")
-                  .data(state.features)
+  Statesvg.selectAll("path")
+    .data(state.features)
+    .join("path")
+    .transition()
+    .duration(1000)
+      .attr("fill", "#b8b8b8")
+        .attr("d", d3.geoPath()
+            .projection(projection2)
+        )
+    .style("stroke", "black")
+    .style("opacity", .3)     
 
-  stateplot
-  .join("path")
-  .transition()
-  .duration(1000)
-    .attr("fill", "#b8b8b8")
-      .attr("d", d3.geoPath()
-          .projection(projection2)
-      )
-  .style("stroke", "black")
-  .style("opacity", .3)     
-  
+
+      // Add circles:
+      Statesvg.selectAll("myCircles")
+      .data(table)
+      .join("circle")
+        .attr("class" , d => "state"+ d.STATE)
+        .attr("cx", function(d){ return projection2([d.LONGITUDE, d.LATITUDE])[0] })
+        .attr("cy", function(d){ return projection2([d.LONGITUDE, d.LATITUDE])[1] })
+        .attr("r", 0)
+        .style("fill", function(d){ return color(d.VALUE) })
+        .attr("stroke", function(d){ return color(d.VALUE) })
+        .attr("stroke-width", 3)
+        .attr("fill-opacity", .4)
+      .on("mouseover", function(event, d) {
+          Tooltip.style("opacity", 1);
+          Tooltip.style("display", "block");
+        })
+      .on("mousemove", function(event, d) {
+          Tooltip
+            .html("Fire year: " + d.FIRE_YEAR + "<br>Acres burned: " + d.FIRE_SIZE + "<br>Cause of fire: "+ d.NWCG_GENERAL_CAUSE+"<br>State: " + d.STATE + "<br>County: " + d.FIPS_NAME)
+            .style("left", (event.x)+5 + "px")
+            .style("top", (event.y)-5 + "px")
+        })
+      .on("mouseleave", function(event, d) {
+          Tooltip.style("opacity", 0);
+          Tooltip.style("display", "none");
+        })
+    
+    var currentSelection = "Texas"
   // A function that update the chart
     function updateState(selectedGroup) {
 
       // Create new data with the selection?
       state.features = data.features.filter(function(d){return d.properties.NAME==selectedGroup})
 
-      stateplot = Statesvg.selectAll("path")
-                  .data(state.features)
+      var proj = d3.geoMercator()
+      .center([stateProjs[selectedGroup][0], stateProjs[selectedGroup][1]])                // GPS of location to zoom on
+      .scale(stateProjs[selectedGroup][2])                       // This is like the zoom
+      .translate([ width/2, height/2 ])
 
-      stateplot
-      .join("path")
-      .transition()
-      .duration(1000)
-        .attr("fill", "#b8b8b8")
-          .attr("d", d3.geoPath()
-              .projection(projection2)
-          )
-      .style("stroke", "black")
-      .style("opacity", .3)      
-      // Give these new data to update line
+      Statesvg.selectAll("path")
+        .data(state.features)
+        .join("path")
+        .transition()
+        .duration(1000)
+          .attr("fill", "#b8b8b8")
+            .attr("d", d3.geoPath()
+                .projection(proj)
+            )
+        .style("stroke", "black")
+        .style("opacity", .3) 
+
+      // 
+      console.log(selectedGroup)
+
+      var e = document.getElementById("stateSelect");
+      var strUser = e.value;
+      console.log(strUser)
+
+      let grp = statesDict[selectedGroup]
+
+      Statesvg.selectAll("myCircles")
+        .attr("class" , d => "state"+ d.STATE)
+        
+        .attr("r", 0)
+        .style("fill", function(d){ return color(d.VALUE) })
+        .attr("stroke", function(d){ return color(d.VALUE) })
+        .attr("stroke-width", 3)
+        .attr("fill-opacity", .4)
+
+      console.log("group")
+      console.log(grp)
+      Statesvg.selectAll(".state"+grp)
+        .attr("cx", function(d){ return proj([d.LONGITUDE, d.LATITUDE])[0] })
+        .attr("cy", function(d){ return proj([d.LONGITUDE, d.LATITUDE])[1] })
+        .transition()
+        .duration(1000)
+        .style("opacity", 1).attr("r", function(d){ return size(d.FIRE_SIZE) })
+
     }
 
-    updateState("Alabama")
+    updateState("Texas")
 
     // When the button is changed, run the updateChart function
     d3.select("#stateSelect").on("change", function(event,d) {
+        console.log("In on change")
+        console.log("currentselection is")
         // recover the option that has been chosen
+        console.log(statesDict[currentSelection])
+        Statesvg.selectAll(".state"+statesDict[currentSelection]).transition().duration(1000).style("opacity", 0).attr("r", 0)
         const selectedOption = d3.select(this).property("value")
         console.log(d)
         console.log(event)
         // run the updateChart function with this selected option
+        console.log("leaving on change")
+        currentSelection = selectedOption
         updateState(selectedOption)
     })
 
